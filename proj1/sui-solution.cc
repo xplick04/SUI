50c30b475f1ae2ddb6d7e6c4f78ece987260acc1
xplick04 <xplick04@fit.vutbr.cz>
2023-10-22 20:06:25 +0200

    Astar Heur

--- proj1/sui-solution.cc
+++ proj1/sui-solution.cc
@@ -9,7 +9,9 @@
 #include <queue>
 #include <set>
 #include <vector>
-
+#include <iostream>
+#include <sstream>
+#include <string>
 
 class Path { 
   public: 
@@ -106,15 +108,93 @@ std::vector<SearchAction> DepthFirstSearch::solve(const SearchState &init_state)
 	return {};
 }
 
+
+void getCardValue(std::string card, int& value, int& color)
+{
+	int i = 1;
+	if(card[0] == 'K') value = 13;
+	else if(card[0] == 'Q') value = 12;
+	else if(card[0] == 'J') value = 11;
+	else if(card[0] == '1' && card[1] == '0') 
+	{
+		value = 10;
+		i = 2;
+	}
+	else value = card[0] - '0'; ;
+
+	if(card[i] == 'h' || card[i] == 'd') color = 0;
+	else color = 1;
+}
+
+
+
 double StudentHeuristic::distanceLowerBound(const GameState &state) const {
     int cards_out_of_home = king_value * colors_list.size();
+
+	int cards_in_home = 0;
     for (const auto &home : state.homes) {
         auto opt_top = home.topCard();
         if (opt_top.has_value())
-            cards_out_of_home -= opt_top->value;
+            cards_in_home += opt_top->value;
     }
 
-    return cards_out_of_home;
+	int freeCells_occupied = 0;
+	for (const auto &FreeCell : state.free_cells) {
+        auto opt_top = FreeCell.topCard();
+        if (opt_top.has_value())
+            freeCells_occupied += 1;
+    }
+
+	
+	int cards_in_wrong_order = 0;
+	for (const auto &stack : state.stacks) {
+		std::ostringstream outputStream;
+		outputStream << stack;
+		std::istringstream inputStream(outputStream.str());
+		std::string item;
+		inputStream >> item;
+		if(item != "_")
+		{
+			int val, col, prevVal, prevCol;
+			getCardValue(item, prevVal, prevCol);
+			while(inputStream >> item)
+			{
+				getCardValue(item, val, col);
+				if(!(prevVal - val == 1 && prevCol == col)) cards_in_wrong_order++;
+				
+				prevVal = val;
+				prevCol = col;
+			}
+		}
+    }
+
+
+	int lowest_card_distance = 0;
+    for (const auto &stack : state.stacks) {
+        std::ostringstream outputStream;
+        outputStream << stack;
+        std::istringstream inputStream(outputStream.str());
+        std::string item;
+        inputStream >> item;
+        if(item != "")
+        {
+            int val, col;
+            int min, minDistance = 14;
+            while(inputStream >> item)
+            {
+                getCardValue(item, val, col);
+                if(val < min)
+                {
+                    min = val;
+                    minDistance = 0;
+                }
+                minDistance++;
+            }
+        lowest_card_distance += minDistance;
+        }
+    }
+	
+	return (cards_out_of_home - cards_in_home) + freeCells_occupied + (0.05 * cards_in_wrong_order) + (0.05 * lowest_card_distance);
 }
 
 
@@ -139,7 +219,6 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
 	std::set<SearchState> closed;
 
 	open.push(new State(init_state, init_state.actions()[0], compute_heuristic(init_state, *heuristic_), nullptr));
-
 	while (!open.empty())
 	{
 		auto current = open.top();
