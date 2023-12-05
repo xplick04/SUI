acd41e973c0c83e65f71798b1e142aa6ef48e31a
xplick04 <maxim.plicka@icloud.com>
2023-12-05 23:58:21 +0100

    fixes

--- proj3/python.py
+++ proj3/python.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-arr = np.random.rand(1, 3, 1, 4)
+arr = np.random.rand(1, 1)
 print("Original array shape:", arr.shape)
 
-arr_squeezed = arr.squeeze()
+arr_squeezed = arr.squeeze(-1)
 print("Squeezed array shape:", arr_squeezed.shape)
